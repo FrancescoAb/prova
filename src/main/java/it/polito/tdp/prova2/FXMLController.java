914cf1b31060f3a5f35978ee35686f5e44f6abc1
utente <utente@172.21.105.8>
2022-03-07 09:18:04 +0100

    versione iniziale

---
+++ src/main/java/it/polito/tdp/prova2/FXMLController.java
@@ -0,0 +1,28 @@
+package it.polito.tdp.prova2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+
+    @FXML
+    private Label txtMessaggio;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    void handleBuongiorno(ActionEvent event) {
+    	//System.out.println("Bottone Premuto");
+    	String nome = txtNome.getText();
+    	if (nome.length()==0) {
+    		txtMessaggio.setText("Per favore inserisci il tuo nome");
+    	} else{
+    	txtMessaggio.setText("Buongiorno, "+nome+".");
+    	txtNome.setText("");
+    	}
+    }
+
+}
